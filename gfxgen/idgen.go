92777ed4dd710b3143993ef923a47d56bdf1dee6
CorpoWolf <gmangavin1@gmail.com>
2024-11-08 15:50:59 -0800

    AddTextLabel function created

--- gfxgen/idgen.go
+++ gfxgen/idgen.go
@@ -2,10 +2,20 @@ package gfxgen
 
 import (
 	"image"
+	"image/color"
+	"image/draw"
 	"log"
+
+	"golang.org/x/image/font"
+	"golang.org/x/image/math/fixed"
 )
 
-func CardID(inputs ...interface{}) {
+func CardTemplate(templateImg image.Image, inputs ...interface{}) (image.Image, error) {
+
+	//Setup image size from template image
+	outputImg := image.NewRGBA(templateImg.Bounds())
+	draw.Draw(outputImg, templateImg.Bounds(), templateImg, image.Point{}, draw.Src)
+
 	for _, input := range inputs {
 		switch v := input.(type) {
 		case image.Image:
@@ -17,4 +27,19 @@ func CardID(inputs ...interface{}) {
 		}
 		log.Printf("potato")
 	}
+
+	return nil, nil
+}
+
+func AddTextLabel(img *image.RGBA, textFace font.Face, x, y int, label string) {
+	col := color.Black
+	point := fixed.Point26_6{X: fixed.I(x), Y: fixed.I(y)}
+
+	d := &font.Drawer{
+		Dst:  img,
+		Src:  image.NewUniform(col),
+		Face: textFace,
+		Dot:  point,
+	}
+	d.DrawString(label)
 }
